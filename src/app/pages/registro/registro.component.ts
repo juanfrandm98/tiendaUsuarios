f2207094189e865bf8c46228debfd5b13e4d7746
Juan Francisco Díaz <juanfrandm98@gmail.com>
2023-05-31 14:21:50 +0200

    Registro e inicio Servicio

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Persona } from 'src/app/model/persona';
+import { ServicioUsuariosService } from 'src/app/services/servicio-usuarios.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  usuario: Persona = new Persona(0, "admin@gmail.com", "admin", "Prueba", "Probador", 546987123);
 
+  constructor(private servicioUsuarios: ServicioUsuariosService) { }
+
+  registrarUsuario() {
+    this.servicioUsuarios.addUsuario(this.usuario);
+  }
 }
--- src/app/services/servicio-usuarios.service.ts
+++ src/app/services/servicio-usuarios.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Persona } from '../model/persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioUsuariosService {
 
-  constructor() { }
+  constructor() {}
+
+  lista: Array<Persona> = [];
+
+  getListaUsuarios(route: ActivatedRoute) {
+    return route.snapshot.paramMap.get('listaUsuarios');
+  }
+
+  addUsuario(nuevoUsuario: Persona) {
+    this.lista = JSON.parse(localStorage.getItem('listaUsuarios')!);
+
+    if(this.lista) {
+      this.lista.push(nuevoUsuario);
+    } else {
+      this.lista = [nuevoUsuario];
+    }
+    
+    localStorage.setItem('listaUsuarios', JSON.stringify(this.lista));
+  }
+
+  login(email: string, contrasenia: string) {
+    this.lista = JSON.parse(localStorage.getItem('listaUsuarios')!);
+  }
 }
